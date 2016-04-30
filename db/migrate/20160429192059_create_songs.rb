b0e6540db63db7bf40d203c1eab7a3944ad53b8a
yugyo <back1land@gmail.com>
2016-04-30 09:29:27 +0900

    First commit

---
+++ db/migrate/20160429192059_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :display_artist
+      t.integer :ranking
+      t.integer :year
+
+      t.timestamps null: false
+    end
+  end
+end
